14f5f390ea94a0e40199739ce8bc3405738a13fd
Steve Helms <steve@cadenrock.com>
2020-02-03 12:19:39 -0600

    Added get_data function skeleton.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import from requests
+
+
 print("Hello World!")
+
+
+def get_data(url):
+  """This function will gather remote data and return a JSON dictionary
+
+  Parameters:
+  url (str): The URL to gather data from
+
+  Returns:
+  dict: A dictionary of the retrieved JSON data
+
+  """
+  return {}
