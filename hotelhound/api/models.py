bf5f2dfb922162f23dd1742a990e804fd524b559
Steve Helms <steve@cadenrock.com>
2020-02-21 17:09:09 -0600

    Updating model to allow empty fields.

--- hotelhound/api/models.py
+++ hotelhound/api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from datetime import datetime
+import time
 
 # Create your models here.
 class Hotel(models.Model):
     name = models.CharField(max_length=128)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=24)
+    address = models.CharField(max_length=255,blank=True)
+    phone_number = models.CharField(max_length=24,blank=True)
     vicinity = models.CharField(max_length=255)
     types = models.CharField(max_length=255)
     google_place_id = models.CharField(max_length=64)
@@ -15,6 +16,18 @@ class Hotel(models.Model):
 
     def __str__(self):
         return str(self.name.title())
+
+    @property
+    def lastupdated(self):
+        seconds_ago = datetime.now() - self.updated_at.replace(tzinfo=None)
+        if seconds_ago > datetime.timedelta(60*60*24*7): # 7 days
+            ago = "over a week"
+        else:
+            day = timedelta(days=1)
+            days_ago = self.updated_at // (24 * 3600)
+            ago = str(f"{days_ago} days")
+
+        return str(f"{ago} ago")
 
 
 class Rating(models.Model):
