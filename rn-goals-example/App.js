d8c0265a019614f6f4586b5541a0e91986b81268
Steve Helms <steve@cadenrock.com>
2020-03-07 11:05:19 -0600

    Adding react native goals app example

---
+++ rn-goals-example/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal}]);
+  };
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "#fff",
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    borderColor: "#000",
+    borderWidth: 1,
+    width: "80%",
+    padding: 10
+  },
+  listItem: {
+    backgroundColor: "#999",
+    borderColor: "#333333",
+    marginTop: 10,
+    borderWidth: 1,
+    padding: 10
+  }
+});
