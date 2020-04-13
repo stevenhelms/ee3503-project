75fe899e3b61f66f5ddf5cb0206fe81f906fcfb2
Steve Helms <steve@cadenrock.com>
2020-04-13 15:45:23 -0500

    Added Redux-Thunk

--- hhmobile/App.js
+++ hhmobile/App.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import ReduxThunk from 'redux-thunk';
 
 import hotelsReducer from "./store/reducers/hotels";
 import HoundNavigator from './components/HoundNavigator';
@@ -12,7 +13,7 @@ const rootReducer = combineReducers({
   hotels: hotelsReducer
 });
 
-const dataStore = createStore(rootReducer);
+const dataStore = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchData = () => {
   return Font.loadAsync({
